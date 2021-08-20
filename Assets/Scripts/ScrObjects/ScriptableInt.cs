e16305c7c97a2902b8bf31f77481709a895e79d3
Kirill Zhilinskiy <kirillzelen00@gmail.com>
2021-08-20 19:07:33 +0200

    Добавлен запоминающий скрипт, и запоминаемое кол-во монстров

---
+++ Assets/Scripts/ScrObjects/ScriptableInt.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Remember Int", menuName = "Remember Int")]
+public class ScriptableInt : ScriptableObject
+{
+    public int value;
+}
