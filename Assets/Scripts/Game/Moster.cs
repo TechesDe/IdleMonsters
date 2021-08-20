2ba593a77b807bbe80f9da3a0779205c4172c423
Kirill Zhilinskiy <kirillzelen00@gmail.com>
2021-08-20 18:27:07 +0200

    Добавлено движение

--- Assets/Scripts/Game/Moster.cs
+++ Assets/Scripts/Game/Moster.cs
@@ -10,9 +10,16 @@ public class Moster : MonoBehaviour {
     private EventListener _updateEvent; //Using to Update or Pause
     //[SerializeField]
     //private EventListener _fixedUpdateEvent; //Using to Fixed Update or Pause
-    
+
+    public Transform field;
+
     public int hp;
     public int bornHp = 100;
+    public float speed = 1f;
+
+    public Vector3 target; //Place where object going
+
+    public float fault = 1; //delta distance to change target
 
     [SerializeField]
     private Slider _hpSlider;
@@ -21,6 +28,7 @@ public class Moster : MonoBehaviour {
         if (bornHp <= 0)
             Destroy(gameObject);
         hp = bornHp;
+        randomTarget();
     }
 
     public void setHP(int value,bool isBorn = false) {
@@ -46,15 +54,32 @@ public class Moster : MonoBehaviour {
 
 
     private void UpdateBehaviour() {
-        
+        #region Move
+        float distance = (transform.position.x - target.x) * (transform.position.x - target.x) + (transform.position.y - target.y) * (transform.position.y - target.y);
+        if (distance < fault) {
+            randomTarget();
+        } else {
+            float x = transform.position.x + Time.deltaTime * speed * ((target.x - transform.position.x) / distance);//TODO: moving without accseleration
+            float y = transform.position.y + Time.deltaTime * speed * ((target.y - transform.position.y) / distance);
+            transform.position = new Vector3(x, y, transform.position.z);
+        }
+        #endregion Move
     }
 
     private void OnMouseDown() {
         hp--;
-
+        if (hp <= 0) {
+            //TODO: Destroy
+        }
         _hpSlider.value = (float)hp / bornHp;
         Text text=_hpSlider.GetComponentInChildren<Text>();
         text.text = hp + "/" + bornHp;
+    }
+
+    private void randomTarget() {
+        float x = Random.Range(field.position.x - field.localScale.x / 2 + transform.localScale.x / 2, field.position.x + field.localScale.x / 2 - transform.localScale.x / 2);
+        float y = Random.Range(field.position.y - field.localScale.y / 2 + transform.localScale.y / 2, field.position.y + field.localScale.y / 2 - transform.localScale.y / 2);
+        target = new Vector3(x, y, transform.position.z);
     }
 
 }
