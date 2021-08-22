fb8d92d0d9f7d10e6dfc7dd613d7510a5e3b7235
Kirill Zhilinskiy <kirillzelen00@gmail.com>
2021-08-22 15:06:44 +0200

    GameManager слушает событие Restart

--- Assets/Scripts/Managers/GManager.cs
+++ Assets/Scripts/Managers/GManager.cs
@@ -16,15 +16,22 @@ public class GManager : MonoBehaviour
     public int lvlDamage = 1;
 
     [SerializeField]
-    private Text lvlText;
+    private Text _lvlText;
+
+    [SerializeField]
+    private GamePanel _gamePanel;
 
     public static GManager Instance;
+
 
     [SerializeField]
     private ScriptableInt monsterCount;
 
     [SerializeField]
     private EventListener _update;
+
+    [SerializeField]
+    private EventListener _restart;
 
     [SerializeField]
     private EventDispatcher _gameOver;
@@ -40,29 +47,34 @@ public class GManager : MonoBehaviour
 
     private void OnEnable() {
         _update.OnEventHappened += IncreasingDifficulty;
+        _restart.OnEventHappened += Restart;
     }
 
     private void OnDisable() {
         _update.OnEventHappened -= IncreasingDifficulty;
+        _restart.OnEventHappened -= Restart;
     }
 
     private void IncreasingDifficulty() {
         if(difficulty<MaxDifficulty)
             difficulty += speedOfDifficulty * Time.deltaTime;
         if (monsterCount.value > 10) {
+            UpdateManager.Instance.PauseToogle(true);
             _gameOver.Dispatch();
         }
     }
 
     public void DamageUp(int value = 1) {
         lvlDamage += value;
-        lvlText.text = lvlDamage.ToString();
+        _lvlText.text = lvlDamage.ToString();
     }
 
     public void Restart() {
         difficulty = 1f;
         lvlDamage = 1;
-        lvlText.text = lvlDamage.ToString();
+        _lvlText.text = lvlDamage.ToString();
         monsterCount.value = 0;
+        _gamePanel.gameObject.SetActive(true);
+        UpdateManager.Instance.PauseToogle(false);
     }
 }
